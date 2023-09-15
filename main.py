8bd82bfd508569c05fbd16961883f9b2dee7a38d
Towet-Tum <daimacvdl@gmail.com>
2023-09-15 17:02:51 +0300

    Data ingestion complete

---
+++ main.py
@@ -0,0 +1,13 @@
+from IntelligentRadiologistAssistant import logger
+from IntelligentRadiologistAssistant.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started >>>>>>")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed >>>>>>>")
+except Exception as e:
+    logger.exception(e)
+    raise e 
--- template.py
+++ template.py
@@ -21,7 +21,9 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html",
+
 ]
 
 for filepath in list_of_files:
